9011bb446ef86406c31b9d0b02952f489371e147
raymond cuya <raymondcuya@gmail.com>
2022-02-02 21:14:11 +1100

    handle mission launches

--- server/src/models/launch.model.js
+++ server/src/models/launch.model.js
@@ -7,13 +7,17 @@ const launch = {
   mission: 'Kepler Exporation X',
   rocket: 'Explorer IS1',
   launchDate: new Date('December 27, 2030'),
-  destination: 'Kepler-442 b',
+  target: 'Kepler-442 b',
   customer: ['SpaceX', 'NASA'],
   upcoming: true,
   success: true,
 };
 
 launches.set(launch.flightNumber, launch);
+
+function existsLaunchWithId(launchId) {
+  return launches.has(launchId);
+}
 
 function getAllLaunches() {
   return Array.from(launches.values());
@@ -32,7 +36,16 @@ function addNewlaunch(launch) {
   );
 }
 
+function abortLaunchById(launchId) {
+  const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+  return aborted;
+}
+
 module.exports = {
+  existsLaunchWithId,
   getAllLaunches,
   addNewlaunch,
+  abortLaunchById,
 };
