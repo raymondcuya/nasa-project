317228c6467a8e56087ab4ad9b7fbea275ac7cf8
Raymond Cuya <raymondcuya@gmail.com>
2022-02-24 21:10:03 +1100

    updated to get launches and planets through mongoose

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,4 +1,5 @@
-// const luanches = require('./lauches.mongo');
+const launchesDatabase = require('./lauches.mongo');
+const planets = require('./planets.mongo');
 
 const launches = new Map();
 
@@ -15,14 +16,34 @@ const launch = {
   success: true,
 };
 
-launches.set(launch.flightNumber, launch);
+saveLaunch(launch);
 
 function existsLaunchWithId(launchId) {
   return launches.has(launchId);
 }
 
-function getAllLaunches() {
-  return Array.from(launches.values());
+async function getAllLaunches() {
+  return await launchesDatabase.find({}, { _id: 0, __v: 0 });
+}
+
+async function saveLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error('No matching planet found');
+  }
+
+  await launchesDatabase.updateOne(
+    {
+      flightNumber: launch.flightNumber,
+    },
+    launch,
+    {
+      upsert: true,
+    }
+  );
 }
 
 function addNewlaunch(launch) {
