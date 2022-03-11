0ef34af1e1600980ac451f035b2ee0c195699e73
Raymond Cuya <raymondcuya@gmail.com>
2022-03-11 19:38:01 +1100

    use process env for mongo

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL =
-  'mongodb+srv://nasa-api:test@nasacluster.vbz4m.mongodb.net/nasa?retryWrites=true&w=majority';
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!');
